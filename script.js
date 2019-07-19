c4dce7b753355087598673909cd370325dea7dbe
Matheus Farias <math.systems.dev@gmail.com>
2019-07-19 14:10:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ $(document).ready(function (){
     $('a').attr('target','_blank');
 });
 
+var lblNome = document.getElementById('seuNome');
+var lblEmail = document.getElementById('exampleFormControlInput1');
+var lblMenssagem = document.getElementById('exampleFormControlTextarea1');
+var btnElement = document.getElementById('btn-send');
+
+console.log(lblNome);
+console.log(lblEmail);
+console.log(lblMenssagem);
+console.log(btnElement);
+
+//Quando clicar no botão de enviar:
+$("#btn_send").click(function(){
+    alert(
+            "Dados coletados para enviar ao e-mail ou banco de dados = \n"+
+            "Nome: "+lblNome.value+"\n"+
+            "Email: "+lblEmail.value+"\n"+
+            "Menssagem: "+lblMenssagem.value
+        );
+  });
